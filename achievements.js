8bece6ad279e97a9f5a79f05bacb62717887d9de
Jonah Coffelt <84864544+JonahCoffelt@users.noreply.github.com>
2024-05-09 17:38:34 -0500

    Add files via upload

--- achievements.js
+++ achievements.js
@@ -2,17 +2,32 @@
 let btn1 = document.getElementById("b1");
 let btn2 = document.getElementById("b2");
 let btn3 = document.getElementById("b3");
+let btn4 = document.getElementById("b4");
+
+let img1 = document.getElementById("i1");
+let img2 = document.getElementById("i2");
+let img3 = document.getElementById("i3");
 
 // Add an event handler for the click event
 btn1.addEventListener("click", button1);
 btn2.addEventListener("click", button2);
 btn3.addEventListener("click", button3);
+btn4.addEventListener("click", reset);
+
+img1.addEventListener("click", button1);
+img2.addEventListener("click", button2);
+img3.addEventListener("click", button3);
 
 function clear(){
     document.getElementById("c1").style.opacity = "0%";
     document.getElementById("c2").style.opacity = "0%";
     document.getElementById("c3").style.opacity = "0%";
     document.getElementById("g").style.opacity = "0%";
+}
+
+function reset() {
+    clear()
+    document.getElementById("g").style.opacity = "100%";
 }
 
 function button1(){
--- projects.js
+++ projects.js
@@ -5,6 +5,14 @@ let btn3 = document.getElementById("b3");
 let btn4 = document.getElementById("b4");
 let btn5 = document.getElementById("b5");
 let btn6 = document.getElementById("b6");
+let btn7 = document.getElementById("b7");
+
+let img1 = document.getElementById("i1");
+let img2 = document.getElementById("i2");
+let img3 = document.getElementById("i3");
+let img4 = document.getElementById("i4");
+let img5 = document.getElementById("i5");
+let img6 = document.getElementById("i6");
 
 // Add an event handler for the click event
 btn1.addEventListener("click", button1);
@@ -13,7 +21,14 @@ btn3.addEventListener("click", button3);
 btn4.addEventListener("click", button4);
 btn5.addEventListener("click", button5);
 btn6.addEventListener("click", button6);
+btn7.addEventListener("click", reset);
 
+img1.addEventListener("click", button1);
+img2.addEventListener("click", button2);
+img3.addEventListener("click", button3);
+img4.addEventListener("click", button4);
+img5.addEventListener("click", button5);
+img6.addEventListener("click", button6);
 
 function clear(){
     document.getElementById("c1").style.opacity = "0%";
@@ -23,6 +38,11 @@ function clear(){
     document.getElementById("c5").style.opacity = "0%";
     document.getElementById("c6").style.opacity = "0%";
     document.getElementById("g").style.opacity = "0%";
+}
+
+function reset() {
+    clear()
+    document.getElementById("g").style.opacity = "100%";
 }
 
 function button1(){
